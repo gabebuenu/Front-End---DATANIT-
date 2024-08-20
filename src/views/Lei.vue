77f8f25f3daa231c1cf96bbaa95e16ef23131e81
Gabriel Bueno <127902855+gabebuenu@users.noreply.github.com>
2024-08-20 19:06:52 -0300

    Update Lei.vue

--- src/views/Lei.vue
+++ src/views/Lei.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="lei-view">
       <iframe 
-        src="https://www.planalto.gov.br/ccivil_03/_ato2004-2006/2004/lei/l10.973.htm" 
+        src="https://www.planalto.gov.br/ccivil_03/_Ato2015-2018/2016/Lei/L13243.htm#art2" 
         width="100%" 
         height="600px"
         style="border:none;">
