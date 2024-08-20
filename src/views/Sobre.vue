37a3f505453ed820e0023839d71cbcb99b1f6866
Gabriel Bueno <127902855+gabebuenu@users.noreply.github.com>
2024-08-20 19:02:56 -0300

    Update Sobre.vue

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -1,108 +1,38 @@
 <template>
-    <div class="container">
-      <section class="about-section">
-        <div class="about-content">
-          <h2>Sobre o DataNit</h2>
-          <p>
-            Lorem ipsum dolor sit amet. Et nisi quia aut deserunt voluptas aut minus repudiandae. Eos officia odio et nihil nihil ut laudantium perspiciatis in alias eius et sint facere rem corporis exercitationem. Est corrupti possimus et maiores beatae quo tempora perferendis in voluptas perspiciatis sed provident esse ut facere natus aut pariatur error. Ut optio deleniti qui ipsam officiis qui iure quos in repellat dolore qui iure quidem.
-          </p>
-        </div>
-        <div class="values-content">
-          <h2>Outras informações</h2>
-          <p>
-            Lorem ipsum dolor sit amet. Et nisi quia aut deserunt voluptas aut minus repudiandae. Eos officia odio et nihil nihil ut laudantium perspiciatis in alias eius et sint facere rem corporis exercitationem. Est corrupti possimus et maiores beatae quo tempora perferendis in voluptas perspiciatis sed provident esse ut facere natus aut pariatur error. Ut optio deleniti qui ipsam officiis qui iure quos in repellat dolore qui iure quidem.
-          </p>
-        </div>
-      </section>
-      <section class="leadership-team">
-        <h2>Sobre os Criadores</h2>
-        <div class="team-members">
-          <div class="team-member">
-            <h3>Gabriel Bueno da Silva</h3>
-            <p>Discente</p>
-            <p>
-                Aut omnis dolorum eum asperiores amet ea voluptate amet 33 temporibus rerum. Qui exercitationem deleniti ut esse officia est fugit dignissimos qui voluptatem veniam et quia quod aut doloremque blanditiis ut amet mollitia.
-            </p>
-          </div>
-          <div class="team-member">
-            <h3>Rafael Gutierres Castanha</h3>
-            <p>Docente</p>
-            <p>
-                Aut omnis dolorum eum asperiores amet ea voluptate amet 33 temporibus rerum. Qui exercitationem deleniti ut esse officia est fugit dignissimos qui voluptatem veniam et quia quod aut doloremque blanditiis ut amet mollitia.
-            </p>
-          </div>
-        </div>
-      </section>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'LeadershipTeam'
-  };
-  </script>
-  
-  <style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap');
-  
-  .container {
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  
-  .about-section {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 50px;
-    text-align: center;
-  }
-  
-  .about-content, .values-content {
-    width: 100%;
-    max-width: 600px;
-    margin: 10px 0;
-  }
-  
-  h2 {
-    font-size: 24px;
-    margin-bottom: 20px;
-  }
-  
-  p {
-    font-size: 16px;
-    line-height: 1.5;
-  }
-  
-  .leadership-team {
-    text-align: center;
-  }
-  
-  .team-members {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-  }
-  
-  .team-member {
-    flex: 1;
-    min-width: 250px;
-    max-width: 300px;
-    margin: 10px;
-  }
-  
-  .team-member h3 {
-    font-size: 20px;
-    margin-bottom: 10px;
-  }
-  
-  .team-member p {
-    font-size: 14px;
-    line-height: 1.5;
-  }
-  </style>
-  
+  <div class="methodology-container">
+    <h1>Sobre o DATANIT</h1>
+    <p>
+      <p>O DATANIT é uma base de dados técnico-científica que reúne registros sobre os Núcleos de Inovação Tecnológica (NIT) das principais universidades brasileiras. Os NIT são regulamentados pela Lei nº 13.243, de 11 de janeiro de 2016, que estabelece medidas de incentivo à inovação e à pesquisa científica e tecnológica no ambiente produtivo, com o objetivo de promover a capacitação e a autonomia tecnológicas, além de fomentar o desenvolvimento do sistema produtivo nacional e regional. O parágrafo IV do artigo 2º desta lei define o NIT como:</p>
+
+<p>"VI - Núcleo de Inovação Tecnológica (NIT): estrutura instituída por uma ou mais ICTs, com ou sem personalidade jurídica própria, que tenha por finalidade a gestão de política institucional de inovação e por competências mínimas as atribuições previstas nesta Lei."</p>
+
+<p>Com o objetivo de fornecer à comunidade uma base de dados abrangente sobre os NIT, o DATANIT reúne informações como websites, endereços, descrições e redes sociais (Instagram) de diversos NIT. Dessa forma, oferece aos gestores de Instituições de Ensino ou de Instituições Científicas, Tecnológicas e de Inovação a oportunidade de cadastrar os núcleos de suas respectivas instituições. Além disso, o DATANIT pode servir como uma importante fonte de pesquisa científica e consulta pública.</p>
+
+<p>O DATANIT é uma iniciativa independente dos pesquisadores Rafael Gutierres Castanha e Gabriel Bueno.</p>
+    </p>
+  </div>
+</template>
+<style scoped>
+.methodology-container {
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+height: 100vh;
+text-align: left;
+font-family: Arial, sans-serif;
+padding: 0 20px;
+}
+
+h1 {
+font-size: 2em;
+margin-bottom: 20px;
+}
+
+p {
+font-size: 1em;
+line-height: 1.5;
+color: #333;
+max-width: 600px; 
+}
+</style>
